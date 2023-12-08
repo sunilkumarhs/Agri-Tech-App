6bdf0a37215fce5679b042385d90e7431f35c944
sunilkumarhs <sunilkumarhs974117@gmail.com>
2023-12-08 21:50:06 +0530

    Building the app with parcel

--- App.js
+++ App.js
@@ -1,3 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
 const heading = React.createElement("h1", { id: "head", xyz: "abcd" }, [
   "Hello World from React!",
   "Hello World from React!",
